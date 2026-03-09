c9c38786921bac2a0224600c706a1f27c1318865
akhmadmax <akhmadmax@gmail.com>
2026-03-09 13:53:01 +0000

    Fixed missing unsubscribe

--- Assets/Scripts/Core/Input/InputProvider.cs
+++ Assets/Scripts/Core/Input/InputProvider.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using UniRx;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using VContainer.Unity;
 
 namespace Metaforce.Core
 {
-    public class InputProvider : IInputProvider, IStartable
+    public class InputProvider : IInputProvider, IStartable, IDisposable
     {
         private readonly Subject<Vector2> _moveSubject = new();
         private readonly InputActionsReference _inputActions;
@@ -20,9 +21,26 @@ namespace Metaforce.Core
         public void Start()
         {
             _inputActions.EnablePlayerMap();
-            
-            _inputActions.Move.performed += context => _moveSubject.OnNext(context.ReadValue<Vector2>());
-            _inputActions.Move.canceled += _ => _moveSubject.OnNext(Vector2.zero);
+
+            _inputActions.Move.performed += OnMovePerformed;
+            _inputActions.Move.canceled += OnMoveCanceled;
+        }
+
+        public void Dispose()
+        {
+            _inputActions.Move.performed -= OnMovePerformed;
+            _inputActions.Move.canceled -= OnMoveCanceled;
+            _moveSubject.Dispose();
+        }
+
+        private void OnMovePerformed(InputAction.CallbackContext context)
+        {
+            _moveSubject.OnNext(context.ReadValue<Vector2>());
+        }
+
+        private void OnMoveCanceled(InputAction.CallbackContext context)
+        {
+            _moveSubject.OnNext(Vector2.zero);
         }
     }
 }
